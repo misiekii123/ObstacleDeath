bb917075cb96a8f36085e65260deffc0d320e789
misiekii123 <michalpl2008@interia.pl>
2022-02-24 19:47:55 +0100

    High score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,9 +6,28 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public Text ScoreText;
+    public Text HighScoreText;
 
     void Start()
     {
         ScoreText.text = "Score: " + GameManager.instance.score.ToString();
+
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            if(GameManager.instance.score > PlayerPrefs.GetInt("HighScore"))
+            {
+                PlayerPrefs.SetInt("HighScore", GameManager.instance.score);
+                HighScoreText.text = "High score: " + PlayerPrefs.GetInt("HighScore");
+            }
+            else
+            {
+                HighScoreText.text = "High score: " + PlayerPrefs.GetInt("HighScore");
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("HighScore", GameManager.instance.score);
+            HighScoreText.text = "High score: " + PlayerPrefs.GetInt("HighScore");
+        }
     }
 }
